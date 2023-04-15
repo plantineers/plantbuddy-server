0d3ec1609a726aeaa1319dbe21c21b02692ea590
mfloto <60036186+mfloto@users.noreply.github.com>
2023-04-15 18:32:38 +0200

    feat: add configuration (#6)
    * feat: add configuration
    
    * feat: make configuration global
    
    * fix: outdated comment
    
    * fix: json naming convention

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,18 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/plantineers/plantbuddy-server/config"
 	"github.com/plantineers/plantbuddy-server/plant"
 )
 
 func main() {
+	// Read configuration file into a global variable and panic if it fails
+	err := config.InitConfig()
+	if err != nil {
+		panic(err)
+	}
+
 	http.HandleFunc("/v1/plant/", plant.PlantHandler)
 
-	fmt.Println(http.ListenAndServe(":3333", nil))
+	fmt.Println(http.ListenAndServe(fmt.Sprintf(":%d", config.PlantBuddyConfig.Port), nil))
 }
---
+++ config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+// Holds the configuration
+type Config struct {
+	Database Database
+	Port     int `json:"port"`
+}
+
+// Holds the database configuration
+type Database struct {
+	DataSource string `json:"dataSource"`
+	DriverName string `json:"driverName"`
+}
+
+// Holds the global configuration
+var PlantBuddyConfig Config
+
+// Reads the buddy.json file
+func InitConfig() error {
+	file, file_err := ioutil.ReadFile("buddy.json")
+	if file_err != nil {
+		return file_err
+	}
+	json_err := json.Unmarshal(file, &PlantBuddyConfig)
+	if json_err != nil {
+		return json_err
+	}
+	return nil
+}
