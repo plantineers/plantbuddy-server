55e628f12d48b83e3ee2d3fdc61300f775049728
mfloto <60036186+mfloto@users.noreply.github.com>
2023-05-26 19:19:12 +0200

    chore: style guide

--- auth/errors.go
+++ auth/errors.go
@@ -2,20 +2,8 @@ package auth
 
 import "errors"
 
-// Either the username or the password is wrong.
-var ErrWrongCredentials = errors.New("wrong credentials!")
-
-// No credentials were supplied.
-var ErrNoCredentials = errors.New("no credentials supplied!")
-
-// The user does not have the required permissions/role.
-var ErrInsufficientPermissions = errors.New("insufficient permissions!")
-
-// The authorization header is invalid.
-var ErrInvalidAuthHeader = errors.New("invalid authorization header!")
-
-// The root user cannot be deleted.
-var ErrCannotDeleteRoot = errors.New("Cannot delete root user")
-
-// The user already exists.
-var ErrUserAlreadyExists = errors.New("User already exists")
+var ErrWrongCredentials = errors.New("wrong credentials")
+var ErrNoCredentials = errors.New("no credentials supplied")
+var ErrInvalidAuthHeader = errors.New("invalid authorization header")
+var ErrCannotDeleteRoot = errors.New("cannot delete root user")
+var ErrUserAlreadyExists = errors.New("user already exists")
