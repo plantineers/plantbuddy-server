65159c38f4d7151d19cee665dca4b717c5ca52f7
Yannick Kirschen <yannickkirschen@protonmail.com>
2023-05-27 15:46:59 +0200

    feat: associate a controller with a plant-group instead a plant #60

--- sensor/sensor-data-filter.go
+++
@@ -1,9 +0,0 @@
-// Author: Yannick Kirschen
-package sensor
-
-type SensorDataFilter struct {
-	Sensor string // Sensor Type
-	Plant  int64  // Plant ID
-	From   string // ISO 8601
-	To     string // ISO 8601
-}
